b8e129ab71671710f62e5893109d1718c4b6d46f
Angel Segovia Gutierrez <angelsegov06@gmail.com>
2026-06-15 19:56:19 -0700

    Added exercise 2.3 and 2.4

--- Chap2DIY/2.1.py
+++ Chap2DIY/2.1.py
@@ -6,3 +6,9 @@ print(x)
 x = x.strip()
 
 print(x)
+
+name = "Angel"
+
+message = f"Hello {name}, would you like to learn some Python today?"
+
+print(message)
---
+++ Chap2DIY/2.3.py
@@ -0,0 +1,13 @@
+"""
+Use a variable to represent a person's name, and print a message to that person
+Also, use a variable to represent a person's name, and print that person's name in lowercase, uppercase, and title case.
+"""
+name = "Angel"
+
+message = f"Hello {name}, would you like to learn some Python today?"
+
+print(message)
+
+print(name.upper())
+print(name.lower())
+print(name.title())
