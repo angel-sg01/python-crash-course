9952762b5e8001f87e4ff76b5dba755ca0f08555
Angel Segovia Gutierrez <angelsegov06@gmail.com>
2026-06-15 20:02:08 -0700

    Added exercise 2.8

---
+++ Chap2DIY/2.8.py
@@ -0,0 +1,6 @@
+"""
+Assign the value python_notes.txt to a variable called filename, then display filename with the file extension
+"""
+filename = "python_notes.txt"
+
+print(filename.removesuffix(".txt"))
